f71d7910d4af53ccac138bb0129d53ac94da668c
buixuanvu0101-gif <buixuanvu0101@gmail.com>
2026-09-17 14:36:41 +0700

    Update Bai1.c

--- Bai1.c
+++ Bai1.c
@@ -1,26 +1,31 @@
-#define RCC_BASE      0x40021000
-#define GPIOC_BASE    0x40011000
+#include "stm32f1xx.h"
 
-#define RCC_APB2ENR   (*(volatile unsigned int *)(RCC_BASE + 0x18))
-#define GPIOC_CRH     (*(volatile unsigned int *)(GPIOC_BASE + 0x04))
-#define GPIOC_ODR     (*(volatile unsigned int *)(GPIOC_BASE + 0x0C))
-
+// HAM DELAY CO BAN
 __attribute__((noinline)) void delay(volatile unsigned int count) {
     while(count--) {
         __asm__("nop");
     }
 }
 
-int main(void) {
-    RCC_APB2ENR |= (1 << 4);
+// HAM TRONG DE THOA MAN YEU CAU GỌI HAM CUA STARTUP.S
+void SystemInit(void) {
+}
 
-    GPIOC_CRH &= ~(0xF << 20);   
-    GPIOC_CRH |=  (0x2 << 20);   
+int main(void) {
+    // BAT CLOCK CHO PORT C (BIT 4 TRONG THANH GHI APB2ENR)
+    RCC->APB2ENR |= (1 << 4);
+
+    // XOA CAU HINH CU VA THIET LAP CHAN PC13 LA OUTPUT PUSH-PULL
+    GPIOC->CRH &= ~(0xF << 20);   
+    GPIOC->CRH |=  (0x2 << 20);   
 
     while(1) {
-        GPIOC_ODR &= ~(1 << 13);   
-        delay(1000000);           
-        GPIOC_ODR |= (1 << 13);    
-        delay(1000000);           
+        // BAT LED (KEO CHAN PC13 XUONG MUC THAP)
+        GPIOC->ODR &= ~(1 << 13);   
+        delay(1000000);             
+        
+        // TAT LED (DAY CHAN PC13 LEN MUC CAO)
+        GPIOC->ODR |= (1 << 13);       
+        delay(1000000);             
     }
 }
